070218fe9f35b3c65194dd5cbf014cdd1c532592
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-03-13 18:06:57 +0100

    Add Obi-Wan job experience demo data

--- src/obi-wan-info.js
+++ src/obi-wan-info.js
@@ -5,19 +5,36 @@ const obiWanInfo = {
 	position: 'Senior Persuasion Specialist',
 	about:
 		"An international expert in the field of persuasion. Former head coach of Jedi Academy, where he conducted the top-ranked MBA course 'Mastering Influence and Persuasion'.",
-    email: 'obi-wan.kenobi@gmail.com',
-    phone: '07987 654321',
-    street: '19 Dalgleish Cres',
-    city: 'Shrewsbury SY3 9FW',
+	email: 'obi-wan.kenobi@gmail.com',
+	phone: '07987 654321',
+	street: '19 Dalgleish Cres',
+	city: 'Shrewsbury SY3 9FW',
 
 	//Experience
 	jobs: [
 		{
-			company: '',
-			position: '',
-			location: '',
-			period: '',
-			description: '',
+			company: 'Human Rights Without Frontiers International',
+			position: 'Diplomat',
+			location: 'Brussels, Belgium',
+			period: '2015 - currently',
+			description:
+				'Publishing research, sharing information, organizing public events and speaking to political leaders on a particular human rights issues.',
+		},
+		{
+			company: 'Jedi Academy',
+			position: 'Force Persuasion Lecturer',
+			location: 'Dantooine',
+			period: '1973 - 2015',
+			description:
+				"Conducting the top-ranked MBA course 'Mastering Influence and Persuasion', that raised a new generation of emotionally intelligent Masters.",
+		},
+		{
+			company: 'Atradius Collections',
+			position: 'Debt Collector',
+			location: 'Worldwide',
+			period: '1972-1973',
+			description:
+				'Encouraging forgetful people to regulate their liabilities. (Not a dream job, but paid the bills)',
 		},
 	],
 };
