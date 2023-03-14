e832348e17cfb8f5c6c00582018add09d77a923c
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-03-14 18:21:40 +0100

    Add option bar and style it, fix spacing

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import './styles/PersonalInfo/Name.scss';
 import './styles/PersonalInfo/Position.scss';
 import './styles/PersonalInfo/About.scss';
 import './styles/PersonalInfo/ContactInfo.scss';
-import './styles/Experience/Job.scss'
-
+import './styles/Experience/Job.scss';
+import './styles/options-bar/buttons.scss';
+import './styles/options-bar/magnifying-glass.scss';
 
 class App extends Component {
 	constructor(props) {
@@ -22,9 +23,22 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Experience defaultData={obiWanInfo} />
-				<PersonalInfo defaultData={obiWanInfo} />
-				<Photo />
+				<div className='options'>
+					<button className='button-59 erase-btn'>ERASE</button>
+					<button className='button-59 demo-btn'>DEMO</button>
+
+					<span class='material-symbols-outlined' id='magnifying-glass'>
+						zoom_in
+					</span>
+					<button className='button-59'>100%</button>
+					<button className='button-59'>75%</button>
+					<button className='button-59'>50%</button>
+				</div>
+				<div className='cv'>
+					<Experience defaultData={obiWanInfo} />
+					<PersonalInfo defaultData={obiWanInfo} />
+					<Photo />
+				</div>
 			</div>
 		);
 	}
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,24 +14,32 @@ export default class Experience extends Component {
 			<div className='Experience'>
 				<div className='header'>Experience</div>
 
-				{defaultData.jobs.map((job, index) => (
-					<Job key={uniqid()} defaultData={defaultData.jobs[index]} />
-				))}
+				<div className="exp-content">
+					{defaultData.jobs.map((job, index) => (
+						<Job key={uniqid()} defaultData={defaultData.jobs[index]} />
+					))}
+				</div>
 
 				<div className='header'>Education</div>
-				<Job defaultData={defaultData.education} />
+				<div className="exp-content">
+					<Job defaultData={defaultData.education} />
+				</div>
 
 				<div className='header'>Skills</div>
-				<ul>
-					{defaultData.skills.map((skill) => (
-						<li key={uniqid()} className='skill'>
-							{skill}
-						</li>
-					))}
-				</ul>
+				<div className="exp-content">
+					<ul>
+						{defaultData.skills.map((skill) => (
+							<li key={uniqid()} className='skill'>
+								{skill}
+							</li>
+						))}
+					</ul>
+				</div>
 
 				<div className='header'>Hobbies</div>
-				<div className='hobbies'>{defaultData.hobbies}</div>
+				<div className="exp-content">
+					<div className='hobbies'>{defaultData.hobbies}</div>
+				</div>
 			</div>
 		);
 	}
